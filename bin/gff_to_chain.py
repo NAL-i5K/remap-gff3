2bf37fbd50069204ee3786b7136276ee27690d91
dytk2134 <dytk2134@gmail.com>
2018-03-22 17:14:27 -0400

    refactor gff_to_chain

--- bin/gff_to_chain.py
+++ bin/gff_to_chain.py
@@ -10,10 +10,16 @@ import sys
 import re
 import logging
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+if not logger.handlers:
+    lh = logging.StreamHandler()
+    lh.setFormatter(logging.Formatter('%(levelname)-8s %(message)s'))
+    logger.addHandler(lh)
+
 __version__ = '1.0'
 
-
-def fasta_file_sequence_length(fasta_file, logger):
+def fasta_file_sequence_length(fasta_file):
     # get the length of the sequence in the fasta_file
     # sequence_length = {'SequenceID': {'length': Sequence_length, 'state': True}
     # True: finished; False: processing
@@ -43,55 +49,23 @@ def fasta_file_sequence_length(fasta_file, logger):
                 else:
                     sequence_length[sequence_id]['length'] += len(line)
     return sequence_length
+def main(alignment_file, target, query, output):
+    logger.info('Reading target genome assembly: (%s)...\n', target)
+    target_length_dict = fasta_file_sequence_length(target)
 
-if __name__ == '__main__':
-    logger_stderr = logging.getLogger(__name__+'stderr')
-    logger_stderr.setLevel(logging.INFO)
-    stderr_handler = logging.StreamHandler()
-    stderr_handler.setFormatter(logging.Formatter('%(levelname)-8s %(message)s'))
-    logger_stderr.addHandler(stderr_handler)
-    logger_null = logging.getLogger(__name__+'null')
-    null_handler = logging.NullHandler()
-    logger_null.addHandler(null_handler)
-    import argparse
-    from textwrap import dedent
-    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=dedent("""\
+    logger.info('Reading query genome assembly: (%s)...\n', query)
+    query_length_dict = fasta_file_sequence_length(query)
 
-    Quick start:
-    python %(prog)s -t_fa target.fasta -q_fa query.fasta -g example.gff3 -o chain.txt
-    """))
-
-    parser.add_argument('-g', '--gff', type=str, help='NCBI remap GFF3 file, gff3 format', required=True)
-    parser.add_argument('-t_fa', '--target_fasta', type=str, help='target alignment fasta file', required=True)
-    parser.add_argument('-q_fa', '--query_fasta', type=str, help='query alignment fasta file', required=True)
-    parser.add_argument('-o', '--output', type=str, help='output chain format file', required=True)
-    parser.add_argument('-r', '--report', type=str, help='output strand convert report')
-    parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + __version__)
-
-    args = parser.parse_args()
-
-    logger_stderr.info('Reading target fasta file: (%s)...\n', args.target_fasta)
-    target_length_dict = fasta_file_sequence_length(args.target_fasta, logger_stderr)
-    query_length_dict = fasta_file_sequence_length(args.query_fasta, logger_stderr)
-
-    logger_stderr.info('Reading query fasta file: (%s)...\n', args.query_fasta)
-    #error_dict example: {'Emr0001': [[15,16],[13]],'Esf0005': [[17]]}
-    error_dict = {}
-    #line_num_dict example: {3: ['Emr0001','Esf0003'], 15: ['Emr0026']}
-    line_num_dict = {}
     chain_ID = 0
     total_alignments = 0
     filtered_alignments = 0
     removed_alignments = 0
     Size_different = 0
-    out_f = open(args.output, 'w')
-    out_r = None
-    if args.report:
-        out_r = open(args.report, 'w')
 
-    with open(args.gff, 'r') as in_f:
+    out_f = open(output, 'w')
+
+    with open(alignment_file, 'rb') as in_f:
         for line in in_f:
-            Convert_strand = False
             line = line.strip()
             if len(line) != 0:
                 if not line.startswith('#'):
@@ -109,7 +83,7 @@ if __name__ == '__main__':
                     try:
                         tSize = target_length_dict[Target[0]]['length']
                     except KeyError:
-                        logger_stderr.warning('ID: %s not found in the target fasta file' % Target[0])
+                        logger.warning('ID: %s not found in the target fasta file' % Target[0])
                         removed_alignments += 1
                         continue
                     tStrand = Target[3]
@@ -119,7 +93,7 @@ if __name__ == '__main__':
                     try:
                         qSize = query_length_dict[tokens[0]]['length']
                     except KeyError:
-                        logger_stderr.warning('ID: %s not found in the query fasta file' % tokens[0])
+                        logger.warning('ID: %s not found in the query fasta file' % tokens[0])
                         removed_alignments += 1
                         continue
                     qStrand = tokens[6]
@@ -141,8 +115,6 @@ if __name__ == '__main__':
                             qStrand = '-'
                         qStart = qSize - int(tokens[4])
                         qEnd = qSize - int(tokens[3]) + 1
-                        Convert_strand = True
-
 
                     if (tEnd - tStart) == (qEnd - qStart):
                         chain_ID += 1
@@ -153,14 +125,28 @@ if __name__ == '__main__':
                         size = str(tEnd - tStart)
                         out_f.write(size + '\n')
                         out_f.write('\n')
-                        if out_r != None and Convert_strand == True:
-                            out_r.write(' '.join(header) + '\n')
                     else:
                         Size_different += 1
     out_f.close()
-    if out_r != None:
-        out_r.close()
-    logger_stderr.info('Total alignment: %d', total_alignments)
-    logger_stderr.info('Filtered alignment: %d', filtered_alignments)
-    logger_stderr.info('Removed alignment: %d', removed_alignments)
-    logger_stderr.info('size different alignment: %d', Size_different)
+    logger.info('Total alignment: %d', total_alignments)
+    logger.info('Filtered alignment: %d', filtered_alignments)
+    logger.info('Removed alignment: %d', removed_alignments)
+    logger.info('size different alignment: %d', Size_different)
+
+if __name__ == '__main__':
+    import argparse
+    from textwrap import dedent
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=dedent("""\
+
+    Quick start:
+    python %(prog)s -t_fa target.fasta -q_fa query.fasta -g example.gff3 -o chain.txt
+    """))
+
+    parser.add_argument('-a', '--alignment_file', type=str, help='NCBI\'s whole-genome alignments(gff3 format).', required=True)
+    parser.add_argument('-t_fa', '--target_fasta', type=str, help='Target genome assembly', required=True)
+    parser.add_argument('-q_fa', '--query_fasta', type=str, help='Query genome assembly', required=True)
+    parser.add_argument('-o', '--output', type=str, help='output chain format file', required=True)
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s' + __version__)
+
+    args = parser.parse_args()
+    main(alignment_file=args.alignment_file, target=args.target_fasta, query=args.query_fasta, output=args.output)
