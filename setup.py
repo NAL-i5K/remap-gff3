651c1dbcc654b127def413eec4e35d1f9c36b6e5
dytk2134 <dytk2134@users.noreply.github.com>
2018-07-23 13:55:52 -0400

    update the version of gff3tool from 1.3.0 to 1.4.0

--- setup.py
+++ setup.py
@@ -120,7 +120,7 @@ setup(
     install_requires=[
         'gff3==0.3.0',
         'CrossMap==0.2.7',
-        'gff3tool==1.3.0'
+        'gff3tool==1.4.0'
     ],  # Optional
 
     # List additional groups of dependencies here (e.g. development
